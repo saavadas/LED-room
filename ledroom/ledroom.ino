6f3e421e88e495e623791c0acb9c1a8d7495de1e
saavadas <slava_0629@bk.ru>
2023-11-04 19:55:46 +0500

    Changed microled to fastled

--- ledroom/ledroom.ino
+++ ledroom/ledroom.ino
@@ -1,24 +1,14 @@
-#include <microLED.h>
 #include <FastLED.h> // ФЛ для функции Noise
 
-#define LED_AMOUNT 300
 #define BRIGHTNESS 100
-#define STRIP_PIN 13
+#define DATA_PIN 13
+#define NUM_LEDS 300
 
-#define ORDER_RGB    // порядок цветов ORDER_GRB / ORDER_RGB / ORDER_BRG ТУТ МОЖЕШЬ ПОПРОБОВАТЬ РАЗНЫЕ ВАРИАНТЫ ЧТОБЫ ЦВЕТА ОТОБРАЖАЛИСЬ ПО ЧЕЛОВЕЧЕСК
-#define COLOR_DEBTH 3   // цветовая глубина: 1, 2, 3 (в байтах)
-
-LEDdata stripLEDs[LED_AMOUNT];  // буфер ленты ступенек
-microLED strip(stripLEDs, LED_AMOUNT, STRIP_PIN);  // объект лента (НЕ МАТРИЦА) из-за разного количества диодов на ступеньку!
-
-byte curBright = CUSTOM_BRIGHT;
+CRGB leds[NUM_LEDS];
 
 void setup() {
+  FastLED.addLeds<WS2813, DATA_PIN, RGB>(leds, NUM_LEDS);
   Serial.begin(9600);
-  setBrightness(curBright);    // яркость (0-255)
-  clear();
-  show();
-  delay(100);
 
 }
 
@@ -28,13 +18,23 @@ void loop() {
 }
 
 void setBrightness(int brightness) {
-  strip.setBrightness(brightness);
+  FastLED.setBrightness(brightness);
 }
 
 void show() {
-  strip.show();
+  FastLED.show();
 }
 
 void clear() {
-  strip.clear();
+  FastLED.clear();
 }
+
+/*
+  random16()      == random from 0..65535
+  Serial.readBytes( (char*)(&leds[i]), 3); // read three bytes: r, g, and b.
+  leds[i].nscale8( 192);
+  leds[i].fadeToBlackBy( 64 );
+
+
+
+*/
